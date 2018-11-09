1fb0696ad908f4784999f34127eb51a2bf111775
Sebastian Dittrich <sebastian.dittrich@live.de>
2018-11-09 17:03:22 +0100

    enable cors for wss://

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ const app = express(feathers());
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
